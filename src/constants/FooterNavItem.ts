5ed45f4b6c272c696eb7c9fb48ef92e8ae1a795b
shervinmansori@gmail.com <shervinmansouri84@gmail.com>
2025-10-25 15:21:15 +0330

    add the new links of the footer

--- src/constants/FooterNavItem.ts
+++ src/constants/FooterNavItem.ts
@@ -2,23 +2,23 @@ export const navItems = [
     {
         title: "Platform",
         items: [
-            { name: "Projects", link: "/platform/overview" },
-            { name: "Investors", link: "/platform/features" },
-            { name: "Builders", link: "/platform/pricing" },
+            { name: "Projects", link: "/project" },
+            { name: "Investors", link: "/Investors" },
+            { name: "Builders", link: "/builders" },
         ],
     },
     {
         title: "Company",
         items: [
-            { name: "About Us", link: "/company/about" },
-            { name: "Careers", link: "/company/careers" },
-            { name: "Contact", link: "/company/contact" },
+            { name: "About Us", link: "/about-us" },
+            { name: "Careers", link: "/about-us" },
+            { name: "Contact", link: "/about-us" },
         ],
     },
     {
         title: "Learn",
         items: [
-            { name: "Blog", link: "/company/about" },
+            { name: "Blog", link: "/blog" },
             { name: "FAQ", link: "/company/careers" },
             { name: "How InfraFund Works", link: "/company/contact" },
             { name: "Terms & Conditions", link: "/company/contact" },
